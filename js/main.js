efcc757334d79b33fc2f500eafab634313572450
edo6661 <muhammadridho5654@gmail.com>
2025-04-22 00:20:22 +0700

    responsive 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("test");
+const toggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+toggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
